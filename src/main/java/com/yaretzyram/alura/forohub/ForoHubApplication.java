13884b3a00864cf99fc967bd6df2875c1c073874
Yaretzy Zuail Ramírez Bravo <yaretzy.rb.21@gmail.com>
2025-07-18 10:48:19 -0600

    first commit

---
+++ src/main/java/com/yaretzyram/alura/forohub/ForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.yaretzyram.alura.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaretzyram/alura/forohub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaretzyram.alura.forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
